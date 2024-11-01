024f7f71f2e5e789ecce93246715114a483968b8
Cristian Garcia <153207626+Chris20385@users.noreply.github.com>
2024-11-01 15:04:40 -0500

    Update teamassigment.py

--- teamassigment.py
+++ teamassigment.py
@@ -10,9 +10,13 @@ def guessing_game():
 # Generate a random number between 1 and 10
 target_number = random.randint(1, 10)
 guess = None
-attempts = 3  # Set the number of allowed attempts
+attempts = 0  # Number of attempts it took to guess the number
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 10.")
-print("You have 3 attempts to guess the correct number.")
+while True: #Starts loop to allow multiple guesses
+    try:
+        user_guess = int(input("Enter your guess: ")) # Promts user for there guess
+        attempts +=1 #Increase the attempt counter
+
 
