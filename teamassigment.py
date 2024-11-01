24fb111a96eea01cfd470e2c992f0e01a5feb531
Cristian Garcia <153207626+Chris20385@users.noreply.github.com>
2024-11-01 13:18:06 -0500

    Update teamassigment.py

--- teamassigment.py
+++ teamassigment.py
@@ -2,8 +2,12 @@
 # Oct 31, 2024
 # a guessing program that will take a number from the user and guess a randomly generated number
 
-
+# Imports the random module
 import random
 
+def guessing_game():
+
 # Generate a random number between 1 and 10
-target_number = random.randint(1, 10)
+    target_number = random.randint(1, 10)
+    attempts = 0 # Number of attempts to guess
+    print("Welcome to the guessing game. Are you ready!")
