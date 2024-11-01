7601e9b81b393320556184712070dc631001088f
Cristian Garcia <153207626+Chris20385@users.noreply.github.com>
2024-11-01 17:55:53 -0500

    Update teamassigment.py

--- teamassigment.py
+++ teamassigment.py
@@ -6,23 +6,28 @@
 import random
 
 def guessing_game():
-guess_number = random.randint(1, 10) # Generate a random number between 1 and 10
-attempts = 0  # Number of attempts it took to guess the number
+    guess_number = random.randint(1, 10) # Generate a random number between 1 and 10
+    attempts = 0  # Number of attempts it took to guess the number
 
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 10.")
-while True: #Starts loop to allow multiple guesses
-    try:
-        user_guess = int(input("Enter your guess: ")) # Promts user for there guess
-        attempts +=1 #Increase the attempt counter
-        if user_guess == guess_number: # Checks if guess is correct
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 10.")
+    while True: #Starts loop to allow multiple guesses
+        try:
+            user_guess = int(input("Enter your guess: ")) # Promts user for there guess
+            attempts +=1 #Increase the attempt counter
+            if user_guess == guess_number: # Checks if guess is correct
                 print (f"Hurray! You guessed the number {guess_number} in {attempts} attempts.")
                 break # Exits loop only if the guess is correct
-        else:
-            if user_guess < guess_number: # Checks if the guess is lower than the target number
-                print("Go higher, try again")
             else:
+                if user_guess < guess_number: # Checks if the guess is lower than the target number
+                    print("Go higher, try again")
+                else:
                     print("Lower try again")
         except ValueError:
             print("Enter a valid number")
+
+# Loop which allows user to play multiple rounds 
+while True:
+    guessing_game()
+    play_again = input("Do you wish to play again?") .strip() .lower() # Asks user if he wishes to play again
 
